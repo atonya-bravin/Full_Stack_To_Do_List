59e3ba0d65f98f38a92df3817b1a2f0770008cec
atonya-bravin <bravinatonya.brani@gmail.com>
2024-11-16 19:37:03 +0300

    Refactor: Creation of components
    The components have been created for code simplicity and ease in
    readability. The submitBtn component has been created for reusability
    because of the need of its use in multiple pages.

---
+++ src/Components/SignInForm.tsx
@@ -0,0 +1,36 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import SubmitBtn from "./Buttons/SubmitBtn";
+import zod from "zod";
+
+//creation of a zod object to be used in data validation
+const  userInfor = zod.object({
+  email: zod.string().email(),
+  password: zod.string().min(8).max(64),
+});
+
+type userInforType = zod.infer<typeof userInfor>;
+
+const SignInForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: {errors},
+  } = useForm<userInforType>({resolver: zodResolver(userInfor)});
+
+  const onSubmit: SubmitHandler<userInforType> = (data) => console.log(data);
+
+  return(
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Email</label>
+      <input defaultValue="example@gmail.com" {...register("email", {required: true})}/>
+      {errors.email && <span>The email is required</span>}
+      <label>Password</label>
+      <input type="password" {...register("password",  {required: true})}/>
+      {errors.password && <span>The password is required</span>}
+      <SubmitBtn />
+    </form>
+  );
+};
+
+export default SignInForm;
