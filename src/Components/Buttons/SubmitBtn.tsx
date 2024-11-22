f2b458f6362ffe3305797bb423d9144eb34263cd
atonya-bravin <bravinatonya.brani@gmail.com>
2024-11-22 19:56:27 +0300

    refactor: change of file directory
    All the files related to this version have been moved to V1 directory
    for ease in change of app versions.

--- src/Components/Buttons/SubmitBtn.tsx
+++
@@ -1,9 +0,0 @@
-const SubmitBtn = () => {
-    return(
-        <>
-            <input className="text-white font-normal text-[18px] bg-[#2E4DE6] bg-opacity-80 py-[8px] rounded-[4px] w-full mt-[16px]" type="submit" />
-        </>
-    );
-};
-
-export default SubmitBtn;
