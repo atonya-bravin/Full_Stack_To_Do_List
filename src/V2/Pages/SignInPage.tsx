78637f4823b2ec6548b2de5bf789ac83ebc380f1
atonya-bravin <bravinatonya.brani@gmail.com>
2024-11-23 17:23:01 +0300

    feat: Increase of validation parameters in the sign in form
    The validation paramers have been increased and the error messages made
    to be equal to the error that has occured.
    
    closes #25

--- src/V2/Pages/SignInPage.tsx
+++ src/V2/Pages/SignInPage.tsx
@@ -8,8 +8,64 @@ import Logo from "../assets/Logo.png"
 
 //creation of a zod object to be used in data validation
 const  userInfor = zod.object({
-  email: zod.string().email(),
-  password: zod.string().min(8).max(64),
+  email: zod
+    .string()
+    .refine(
+      (value) => value.length > 0, 
+      { message: "Email is required" }
+    )
+    .refine(
+      /*
+        use of regex to check and invalidate anything that;
+          - contains more than one @ symbol
+          - does not contain any characters after the @ symbol
+          - does not contain a dot characters after the characters preceding @symbol
+      */
+      (value) =>
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value),
+      { message: "Invalid email format" }
+  ),
+  password: zod
+    .string()
+    .refine(
+      (value) => value.length > 0,
+      { message: "Password is required" }
+    )
+    .refine(
+      /*
+        Check to invalidate anything with a length less than 8 or greater than 64
+      */
+      (value) => value.length >= 8 && value.length <= 64,
+      { message: "Password must be between 8 and 64 characters" }
+    )
+    .refine(
+      /*
+        Check to invalidate anything without atleast one uppercase
+      */
+      (value) => /[A-Z]/.test(value),
+      { message: "Password must contain at least one uppercase letter" }
+    )
+    /*
+        Check to invalidate anything without atleast one lowercase
+    */
+    .refine(
+      (value) => /[a-z]/.test(value),
+      { message: "Password must contain at least one lowercase letter" }
+    )
+    .refine(
+      /*
+        Check to invalidate anything without atleast one digit [0-9]
+      */
+      (value) => /\d/.test(value),
+      { message: "Password must contain at least one digit" }
+    )
+    .refine(
+      /*
+        Check to invalidate anything without atleast one special character
+      */
+      (value) => /[!@#$%^&*(),.?":{}|<>]/.test(value),
+      { message: "Password must contain at least one special character" }
+  ),
 });
 
 //implementation of a type that reflects the zod object for proper data validation
@@ -43,12 +99,12 @@ const SignInPage = () => {
             <input className="py-[8px] px-[8px] border-[2px] rounded-[4px]"
               placeholder="example@gmail.com" {...register("email", {required: true})}
             />
-            {errors.email && <div className="text-[14px] text-[#DB6353]">The email is required</div>}
+            {errors.email && <div className="text-[14px] text-[#DB6353]">{errors.email.message}</div>}
           </div>
           <div className="flex flex-col gap-[8px] pt-[16px] pb-[8px]">
             <label className="font-normal text-[16px]">Password</label>
             <input className="py-[8px] px-[8px] border-[2px] rounded-[4px]" type="password" {...register("password",  {required: true})}/>
-            {errors.password && <div className="text-[14px] text-[#DB6353]">The password is required</div>}
+            {errors.password && <div className="text-[14px] text-[#DB6353]">{errors.password.message}</div>}
           </div>
 
           <div
