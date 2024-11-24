2495554ddaa1667a66d34154a08ff79d1144e6e3
atonya-bravin <bravinatonya.brani@gmail.com>
2024-11-24 17:50:59 +0300

    feat: change of the tasks link to dashboard
    The dashboard will now be used to show active user tasks.

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
   import HomePage from "./V1/Pages/HomePage"; 
   import SignUpPage from "./V1/Pages/SignUpPage";
   import SignInPage from "./V1/Pages/SignInPage";
-  
+  import TaskPage from "./V1/Pages/TaskPage";
 */
-import TaskPage from "./V1/Pages/TaskPage";
+
 
 
 
@@ -15,6 +15,7 @@ import TaskPage from "./V1/Pages/TaskPage";
 import HomePage from "./V2/Pages/HomePage";
 import SignUpPage from "./V2/Pages/SignUpPage";
 import SignInPage from "./V2/Pages/SignInPage";
+import Dashboard from "./V2/Pages/Dashboard";
 
 const App = () => {
   return(
@@ -26,9 +27,10 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/tasks" element={<TaskPage />} />
+          {/* <Route path="/tasks" element={<TaskPage />} /> */}
           <Route path="/signIn" element={<SignInPage />} />
           <Route path="/SignUp" element={<SignUpPage />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
         </Routes>
       </Router>
     </div>
